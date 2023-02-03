e0c774e69d4ce16843fc9f3bb70c56c7e54a45bd
moha <moha729g@stud.kea.dk>
2023-02-03 10:34:45 +0100

    init commit

---
+++ 00._Miscellaneous/vaiables.js
@@ -0,0 +1,14 @@
+//Const Rule 1: Have to assign a value when declaring a const
+//const me;
+
+console.log("hello world")
+
+const me = {
+    name: "Mo"
+}
+
+//Const Rule 2: cannot reassign a const 
+//me = 2
+
+
+
