499eba993d0d14c7a626a98d7056f35bcaa7f1fd
moha <moha729g@stud.kea.dk>
2023-03-10 09:01:17 +0100

    timeout

--- 03._Plain_HTML/public/frontpage/frontpage.js
+++ 03._Plain_HTML/public/frontpage/frontpage.js
@@ -1,2 +1,14 @@
 //Static file = images, files, css, js
 console.log('hello world');
+
+let timeoutID
+
+function timer() {
+    timeoutID = setTimeout(redirectFunction, 3000)
+}
+
+function redirectFunction(){
+    location = '/tanks'
+}
+
+timer()
